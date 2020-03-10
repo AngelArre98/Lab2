fb1995ccf57939e196a24a9e0edbf9f03edbd849
Angel Vazquez <angel.alberto@live.com.mx>
2020-03-09 18:59:12 -0600

    base añadida

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,7 +3,7 @@ titulo varchar(100),
 repro int,
 url varchar(100))
 
-
+----------------
 create procedure sp_video_insertar
 
 @Idvideo int, @titulo varchar(100), @repro int, @url varchar(100)
@@ -14,5 +14,34 @@ end
 
 exec sp_video_insertar 1,'video 1' ,1,'youtube.com'
 
+-------
+create procedure sp_eliminar
+@IdVideo int
+as
+begin
+delete from video where Idvideo = @IdVideo
+end
+----
+create procedure sp_edit
+@Idvideo int, @titulo varchar(100), @repro int, @url varchar(100)
+as
+begin
+
+update video set Idvideo = @Idvideo, titulo = @titulo, repro = @repro, url = @url where Idvideo = @Idvideo
+end
+----
+
+create procedure sp_mostrar
+as
+begin
+Select*from video;
+end
+
+exec sp_mostrar
+-------------------
+
+
 select * from video
+
+
 
